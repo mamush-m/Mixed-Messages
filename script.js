472b7a2ac1cd1447ac7828d2db1e4c5eedb76d39
mamush-m <svrmamush@gmail.com>
2022-03-14 03:52:31 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("HELLO AND WELCOME TO OUR MIXED MESSAGE GENERATOR. CLICK ON RUN TO SEE A WIDE ARRAY (get it? array) OF MESSAGES.");
+
+let lst = [3, 4, 12, 1, 1, 1, 1, 1, 1, 1, 1];
+console.log(lst);
